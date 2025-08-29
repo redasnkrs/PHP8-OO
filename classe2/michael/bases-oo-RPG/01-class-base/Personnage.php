d992d5976d1f473edfc7df01373a4e57044661f9
WebDevCF2m <gitweb@cf2m.be>
2025-08-29 14:10:47 +0200

    01-class-base

---
+++ classe2/michael/bases-oo-RPG/01-class-base/Personnage.php
@@ -0,0 +1,10 @@
+<?php
+// Le nom de la classe doit correspondre au nom du fichier
+// on déclare une classe avec le mot clef `class{}`
+
+class Personnage
+{
+    // Attributs
+    // Constantes
+    // Méthodes
+}
