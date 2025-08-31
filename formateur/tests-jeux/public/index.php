5a605de9ea61b17d7da9b8203d701e532073cc42
mikhawa <michaeljpitz@gmail.com>
2025-08-31 09:44:17 +0200

    twig install

--- formateur/tests-jeux/public/index.php
+++ formateur/tests-jeux/public/index.php
@@ -1,1 +1,10 @@
 <?php
+
+use Twig\Loader\FilesystemLoader;
+
+require_once '../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('../template');
+$twig = new \Twig\Environment($loader, [
+   // 'cache' => '/path/to/compilation_cache',
+]);
