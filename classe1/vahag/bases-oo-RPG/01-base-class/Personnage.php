4c203b3a6f8d43719217b1fa939aaa17fcfb8b99
vahag1991 <smbatyanvahagn1991@gmail.com>
2025-09-02 13:43:34 +0200

    all good

---
+++ classe1/vahag/bases-oo-RPG/01-base-class/Personnage.php
@@ -0,0 +1,11 @@
+<?php
+
+// La classe est une usine qui va permettre de créer des objets
+// de son type via une instanciation
+// La classe DOIT avoir le même nom que le fichier
+
+class Personnage{
+    // Propriétés (Attributs)
+    // Constantes
+    // Méthodes
+}
