bc028a884d5789f10f4e30c08247c46b0ceadbb3
WebDevCF2m <gitweb@cf2m.be>
2025-09-12 16:22:17 +0200

    exe1 ok

--- classe1/michael/bases-oo-RPG/06-construct-get-set/NotrePerso.php
+++ classe1/michael/bases-oo-RPG/06-construct-get-set/NotrePerso.php
@@ -20,11 +20,12 @@ class NotrePerso{
     // méthodes
 
     // constructeur appelé lors de l'instanciation (new) ! Ajout de la race
-    public function __construct(?int $lid,?string $lename){
+    public function __construct(?int $lid,?string $lename,?string $race){
         // utilisation des setters pour protéger les champs
         $this->setName($lename);
         $this->setId($lid);
         // utilisation du setter de espece_perso
+        $this->setEspecePerso($race);
     }
 
     // getter
@@ -39,8 +40,9 @@ class NotrePerso{
     }
 
     // envoi null ou l'espèce choisie
-    public function getEspecePerso(){
-
+    public function getEspecePerso():?string
+    {
+        return $this->espece_perso;
     }
 
     // setter
@@ -57,12 +59,20 @@ class NotrePerso{
     }
     public function setName(?string $name):void
     {
-        if(!is_null($name))
-            $this->name = htmlspecialchars(strip_tags(trim($name)));
+        if(is_null($name)) return;
+        $name = htmlspecialchars(strip_tags(trim($name)));
+        if(empty($name))
+            throw new Exception("Nom vide !");
+
+        $this->name = $name;
+
     }
     // on ne peut choisir qu'une espèce se trouvant dans self::CHOIX_ESPECE
     public function setEspecePerso(?string $race):void
     {
+        if(!in_array($race,self::CHOIX_ESPECE))
+            throw new Exception("Cette espèce n'est pas encore disponible!");
 
+        $this->espece_perso = $race;
     }
 }
--- classe1/michael/bases-oo-RPG/06-construct-get-set/index.php
+++ classe1/michael/bases-oo-RPG/06-construct-get-set/index.php
@@ -14,26 +14,42 @@ include "NotrePerso.php";
 <body>
 <h1>Création d'un personnage</h1>
 <p>Nous allons créer une instance de NotrePerso()</p>
-
+<form action="" method="post" name="creer">
+    <input type="text" name="name" placeholder="Votre nom"/><br>
+    <select name="espece_perso">
+        <option value="rien">Choisissez un type</option>
+        <?php
+        foreach (NotrePerso::CHOIX_ESPECE as $item):
+        ?>
+            <option value="<?=$item?>"><?=$item?></option>
+        <?php
+        endforeach;
+        ?>
+    </select>
+    <input type="submit" value="créer le personnage"/>
+</form>
+<hr>
 <?php
-try {
-    $perso1 = new NotrePerso(null, "<script>Pierre");
-}catch (Exception $e){
-    die($e->getMessage());
+if(isset($_POST['name'],$_POST['espece_perso'])) {
+    try {
+        $perso1 = new NotrePerso(null, $_POST['name'],$_POST['espece_perso']);
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
+
+    echo "<p>{$perso1->getId()}</p><p>{$perso1->getName()}</p><p>{$perso1->getEspecePerso()}</p>";
+
+    /*
+    echo $perso1->getId();
+    $perso1->setId(65);
+    echo $perso1->getId();
+    echo "<hr>{$perso1->getName()}";
+    $perso1->setName(" <script> alert('jghjg'); </script>    ");
+    echo "<hr>{$perso1->getName()}";
+    */
+    var_dump($perso1);
 }
-
-echo "<p>{$perso1->getId()}</p><p>{$perso1->getName()}</p>";
-
-/*
-echo $perso1->getId();
-$perso1->setId(65);
-echo $perso1->getId();
-echo "<hr>{$perso1->getName()}";
-$perso1->setName(" <script> alert('jghjg'); </script>    ");
-echo "<hr>{$perso1->getName()}";
-*/
-var_dump($perso1);
-
+var_dump($_POST);
 ?>
 </body>
 </html>
