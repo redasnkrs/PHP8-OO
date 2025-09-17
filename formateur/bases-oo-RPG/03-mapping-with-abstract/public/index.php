a9e1cdf2fbfd641e4e1c0b064b85f69b2ea712d2
mikhawa <michaeljpitz@gmail.com>
2025-09-17 13:31:45 +0200

    use static méthode

--- formateur/bases-oo-RPG/03-mapping-with-abstract/public/index.php
+++ formateur/bases-oo-RPG/03-mapping-with-abstract/public/index.php
@@ -1,6 +1,10 @@
 <?php
-declare(strict_types=1);
+
 
 require_once "../config.php";
+require_once "../model/AbstractMapping.php";
 
 echo RACINE_PATH;
+echo "<hr>";
+$titre = "Un titre l'école et l'hôpital";
+echo AbstractMapping::slugify($titre);
