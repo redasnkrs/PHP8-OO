44189166aec6917e4094e1082544ece1a0061240
Mykyta Horban <masarash12345@gmail.com>
2025-09-18 09:52:57 +0200

    text

--- classe2/mykyta/bases-oo-RPG/03-mapping-with-abstractModel/public/index.php
+++ classe2/mykyta/bases-oo-RPG/03-mapping-with-abstractModel/public/index.php
@@ -1,10 +1,13 @@
-<?php 
+<?php
+
 
 require_once "../config.php";
-echo __DIR__;
-require_once "../model/AbstractMapping.php";
 
-// require_once RASINE_PATH."model/abstractMapping.php";
+// utilisation de la racine pour construire nos include, require etc.
+require_once RACINE_PATH."/model/AbstractMapping.php";
 
-echo "<p>Les constantes magiques : <a href=#></a></p>";
-
+echo "<p>Les constantes magiques : <a href='https://www.php.net/manual/en/language.constants.magic.php' target='_blank'>constantes magiques</a> </p>";
+echo "<p>RACINE_PATH chemin fixe vers la racine de notre site : ".RACINE_PATH."</p>";
+echo "<hr><p>Utilisation provisoire d'une méthode statique (echo AbstractMapping::slugify(".'$titre'.");)</p>";
+$titre = "Un titre l'école et l'hôpital";
+echo AbstractMapping::slugify($titre);
