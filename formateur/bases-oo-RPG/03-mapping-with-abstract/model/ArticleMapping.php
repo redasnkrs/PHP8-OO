61566bc99f24424051a454052a5bddf2b61e1b2f
WebDevCF2m <gitweb@cf2m.be>
2025-09-18 10:28:23 +0200

    propriétés de ArticleMapping

---
+++ formateur/bases-oo-RPG/03-mapping-with-abstract/model/ArticleMapping.php
@@ -0,0 +1,20 @@
+<?php
+
+class ArticleMapping{
+    // propriétés = champs de la table
+    protected ?int $id=null;
+    protected ?string $article_title=null;
+    protected ?string $article_slug=null;
+    protected ?string $article_text=null;
+    protected ?string $article_date=null;
+    protected null|bool|int $article_visibility=null;
+
+    // méthodes
+
+    // constructeur
+    public function __construct(){
+        // exemple de texte prouvant l'instanciation
+        echo __CLASS__." instanciée";
+    }
+
+}
