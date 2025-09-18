8ab5bc16c25e5a8a0f6b1699c9f958dcab104457
WebDevCF2m <gitweb@cf2m.be>
2025-09-18 11:01:30 +0200

    exe1 OO

--- formateur/bases-oo-RPG/03-mapping-with-abstract/model/ArticleMapping.php
+++ formateur/bases-oo-RPG/03-mapping-with-abstract/model/ArticleMapping.php
@@ -2,12 +2,12 @@
 
 class ArticleMapping{
     // propriétés = champs de la table
-    protected ?int $id=null;
-    protected ?string $article_title=null;
-    protected ?string $article_slug=null;
-    protected ?string $article_text=null;
-    protected ?string $article_date=null;
-    protected null|bool|int $article_visibility=null;
+    protected ?int $id=null; // entier positif
+    protected ?string $article_title=null; // string de 120 max et 6 minimum sans tags, sans espace devant et derrière, caractères spéciaux encodés
+    protected ?string $article_slug=null; // string de 125 max et 6 minimum sans tags, sans espace devant et derrière, caractères spéciaux encodés
+    protected ?string $article_text=null; // minimum 20 caractères, sans tags, sans espace devant et derrière, caractères spéciaux encodés
+    protected ?string $article_date=null; // doit être une date valide si remplie sinon erreur
+    protected null|bool|int $article_visibility=null; // si int convertir en bool, si bool, attribuer la valeur
 
     // méthodes
 
@@ -18,6 +18,18 @@ class ArticleMapping{
     }
 
     // getters et setters
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id):void
+    {
+        if(is_null($id)) return;
+        if($id<=0)
+            throw new Exception("L'id doit être positif");
+        $this->id = $id;
+    }
 
 
 }
