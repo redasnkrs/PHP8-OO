4dc63b0f3f9b987d660a93680a506a7961c9fd9c
WebDevCF2m <gitweb@cf2m.be>
2025-09-24 11:35:17 +0200

    structure 5

---
+++ formateur/05-interface-trait/controller/routerController.php
@@ -0,0 +1,5 @@
+<?php
+// echo __FILE__; // chemin et nom de fichier
+
+// Page d'accueil
+include RACINE_PATH."/view/homepage.html.php";
--- formateur/05-interface-trait/public/index.php
+++ formateur/05-interface-trait/public/index.php
@@ -1,2 +1,14 @@
 <?php
 declare(strict_types=1);
+
+require_once "../config.php";
+
+// chargement des dépendances dans l'ordre généalogique
+// parcequ'il n'y a pas encore d'autoload
+include RACINE_PATH."/model/SlugifyTrait.php";
+include RACINE_PATH."/model/AbstractMapping.php";// doit être chargé avant ArticleMapping (sinon erreur d'extends)
+include RACINE_PATH."/model/ArticleMapping.php";
+
+// chargement du router
+require_once RACINE_PATH."/controller/routerController.php";
+
