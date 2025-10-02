2cafc0767b8241d193a1876c38fac2e2bf6c2042
mikhawa <michaeljpitz@gmail.com>
2025-10-02 14:45:54 +0200

    prepare update form

--- formateur/06-namespace/view/update.html.php
+++ formateur/06-namespace/view/update.html.php
@@ -16,6 +16,31 @@
         <a href ="./">Accueil</a> | <a href ="./?p=admin">Administration</a> | <a href="?p=create">Création d'un nouvel article</a>
     </nav>
     <h2>Articles de notre site</h2>
+<form action="" method="post" name="name">
+    <input type="hidden" name="article_id" value="<?= $article->getId() ?>">
+        <label for="article_title">Titre de l'article</label><br>
+        <input type="text" name="article_title" id="article_title" value="<?= htmlspecialchars($article->getArticleTitle()) ?>" required><br><br>
+        <label for="article_slug">Slug de l'article</label><br>
+        <input type="text" name="article_slug" id="article_slug" value="<?= htmlspecialchars($article->getArticleSlug()) ?>" required><br><br>
+
+        <label for="article_text">Texte de l'article</label><br>
+        <textarea name="article_text" id="article_text" cols="30" rows="10" required><?= htmlspecialchars($article->getArticleText()) ?></textarea><br><br>
+        <label for="article_date">Date de l'article</label><br>
+        <input type="datetime-local" name="article_date" id="article_date" value="<?= $article->getArticleDate()->format('Y-m-d\TH:i') ?>" required><br><br>
+
+        <label for="article_visibility">Visibilité de l'article</label><br>
+        <select name="article_visibility" id="article_visibility" required>
+            <option value="1" <?= $article->getArticleVisibility() ? 'selected' : '' ?>>Visible</option>
+            <option value="0" <?= !$article->getArticleVisibility() ? 'selected' : '' ?>>Non visible</option>
+        </select><br><br>
+
+        <input type="submit" value="Mettre à jour l'article">
+
+    </form>
+<?php var_dump($_POST);
+// si on a soumis le formulaire
+if(isset($article)) var_dump($article);
+?>
 
 </body>
 </html>
